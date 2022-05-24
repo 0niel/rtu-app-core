a56acdfe109a6b1560b317b9a2da85ca6146770a
Sergey Dmitriev <51058739+0niel@users.noreply.github.com>
2022-05-24 23:19:15 +0300

    Create platform switch button

---
+++ lib/widgets/button/switch_button.dart
@@ -0,0 +1,100 @@
+import 'package:enough_platform_widgets/enough_platform_widgets.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:rtu_app_core/rtu_app_core.dart';
+
+class NinjaSwitchButton extends StatefulWidget {
+  const NinjaSwitchButton({
+    Key? key,
+    required this.icon,
+    required this.text,
+    required this.onChanged,
+    required this.initialValue,
+    this.padding = const EdgeInsets.symmetric(horizontal: 24, vertical: 8),
+  }) : super(key: key);
+
+  final IconData icon;
+  final String text;
+  final Function(bool) onChanged;
+  final bool initialValue;
+  final EdgeInsets padding;
+
+  @override
+  State<NinjaSwitchButton> createState() => _NinjaSwitchButtonState();
+}
+
+class _NinjaSwitchButtonState extends State<NinjaSwitchButton> {
+  late ValueNotifier<bool> _switchValueNotifier;
+
+  @override
+  void initState() {
+    super.initState();
+    _switchValueNotifier = ValueNotifier(widget.initialValue);
+  }
+
+  Widget _buildSwitch() {
+    return ValueListenableBuilder(
+      valueListenable: _switchValueNotifier,
+      builder: (context, hasError, child) => CupertinoSwitch(
+        activeColor: Theme.of(context).colorScheme.secondary,
+        value: _switchValueNotifier.value,
+        onChanged: (value) {
+          _switchValueNotifier.value = value;
+          widget.onChanged(value);
+        },
+      ),
+    );
+  }
+
+  Widget _buildTitle() {
+    return Row(
+      children: [
+        Container(
+          width: 32,
+          height: 32,
+          decoration: const BoxDecoration(
+            color: NinjaConstant.grey100,
+            borderRadius: BorderRadius.all(Radius.circular(50)),
+          ),
+          child: Icon(widget.icon, size: 16),
+        ),
+        const SizedBox(width: 14),
+        NinjaText.bodyMedium(widget.text, fontWeight: 600),
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        MergeSemantics(
+          child: PlatformWidget(
+            cupertino: (_, __) => CupertinoListTile(
+              dense: true,
+              border: const Border(bottom: BorderSide.none),
+              contentPadding: widget.padding,
+              title: _buildTitle(),
+              trailing: _buildSwitch(),
+              onTap: () =>
+                  _switchValueNotifier.value = !_switchValueNotifier.value,
+            ),
+            material: (_, __) => ListTile(
+              dense: true,
+              contentPadding: widget.padding,
+              title: _buildTitle(),
+              trailing: _buildSwitch(),
+              onTap: () =>
+                  _switchValueNotifier.value = !_switchValueNotifier.value,
+            ),
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.only(
+              left: widget.padding.left, right: widget.padding.right),
+          child: const Divider(height: 1),
+        ),
+      ],
+    );
+  }
+}
--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -7,3 +7,4 @@ export 'button/text_button.dart';
 export 'text/text.dart';
 export 'choice_chip/choice_chip.dart';
 export 'bottom_navigation_bar/bottom_navigation_bar.dart';
+export 'button/switch_button.dart';
